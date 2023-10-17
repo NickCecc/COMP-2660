a3c6b7f5d0e3751e04357e55166dde6f8156d945
Nick Cecchin <104159465+NickCecc@users.noreply.github.com>
2023-10-17 22:45:57 +0000

    Added Q3

---
+++ src/Lab4Q3.asm
@@ -0,0 +1,14 @@
+; LAB4Q3
+; ./run.sh Lab4Q3
+; Name: Nicholas Cecchin
+; Date: October 17 2023
+; ID: 110101683
+; Description: Lab 4 Section 53
+;State three main differences when summing an array with Java or C and summing it within assembly. What do you think about the performance?
+;Java is much easier and less code-heavy than assembly
+Java memory is very easy to work with and has garbage collection. Assembly all the memory must be assignmend and accessed manually
+Java has error recognition and will provide suggestions, assembly will not.
+Assembly will be faster if implemented properly and efficiently.
+
+4. What is the benefit of using a register to dereference an array element? Are there any other suggestions?
+Beneficial due to the fast access times of the cpu registers compared to accessing main memory
