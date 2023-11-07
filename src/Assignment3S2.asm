36a5f973b23d7e33d8619dcb36a1fdab839af336
Nick Cecchin <104159465+NickCecc@users.noreply.github.com>
2023-11-07 17:06:14 +0000

    Assignment3S2 Finished

--- src/Assignment3S2.asm
+++ src/Assignment3S2.asm
@@ -18,6 +18,9 @@ INCLUDELIB user32.lib
     resultM BYTE "Sum: ", 0
     sum DWORD ?
 
+    str1 BYTE 0ah, "Colored text on the console", 0
+    colorArr DWORD Red, Green, Blue, Yellow
+
 
 .code
 main PROC
@@ -47,6 +50,54 @@ main PROC
 
 ; Question 2: displays the same string in four different colors, using a loop. 
 ; Call the Set-TextColor procedure from the book’s link library. Any colors may be chosen, but you may find it easiest to change the foreground color.
+
+ mov ecx, LENGTHOF colorArr
+ mov esi, 0
+
+displayStr:
+    push ecx
+    mov eax, [colorArr + esi * TYPE colorArr]
+    call SetTextColor
+
+    mov edx, OFFSET str1
+    call WriteString
+
+    pop ecx
+    add esi, 1
+    loop displayStr
+
+    mov eax, Gray
+    call SetTextColor
+    
+
+; Question 3: program that corresponds to the following flowchart diagram in Figure 2. Indicate in the comments the function of the program.
+
+; Save esi and ecx on the stack
+push esi
+push ecx
+
+; Set eax to 0
+ mov eax, 0
+
+sumLoop:
+    ; Add the pointed-to-value of esi to eax
+    add eax, [esi]
+
+    ; Add 4 to esi which will make esi point to the next DWORD
+    add esi, 4
+
+    ; Decrement ecx
+    dec ecx
+    ; loop sumLoop if ecx is > 0
+    jg sumLoop
+
+    ; Restore original values of ecx and esi
+    pop ecx
+    pop esi
+
+
+
+
 
 
 
